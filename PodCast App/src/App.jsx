86bb00ff89c7b7fc901b586b52c3a39ed3311618
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-15 11:18:25 +0200

    move everything from previous capstone project to this one
    - Create Hero component
    - import all packages like material ui, swiper, router-dom
    
    - create Navbar component
    - create Component folder
    - move all packages from the podcast github

---
+++ PodCast App/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+      <h1 className="heading">LISTEN NOW</h1>
+    </>
+  )
+}
+
+export default App
---
+++ PodCast App/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Swiper, SwiperSlide} from 'swiper'
+
+
+const Hero = () => {
+
+    return (
+
+        <div>
+            
+        </div>
+    )
+}
+
+export default Hero;
